f910a9f5968567de03fc1d8409db3578d56e0afa
willdeuschle <wjdeuschle@gmail.com>
2016-11-08 20:45:38 -0500

    first exercise of ch8

---
+++ Ch08/exercises.js
@@ -0,0 +1,24 @@
+// first exercise
+function MultiplicatorUnitFailure() {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.5)
+    return a * b;
+  else
+    throw new MultiplicatorUnitFailure();
+}
+
+function reliableMultiply(a, b) {
+  // Your code here.
+  for (;;) {
+    try {
+      return primitiveMultiply(a, b);
+    } catch (e) {
+      if (!(e instanceof MultiplicatorUnitFailure))
+        throw e;
+    }
+  }
+}
+
+console.log(reliableMultiply(8, 8));
+// → 64
