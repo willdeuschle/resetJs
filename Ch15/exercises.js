053c5291632b46773ed803ce5fe143e14de2d900
willdeuschle <wjdeuschle@gmail.com>
2016-11-29 22:00:58 -0500

    bonus work

--- Ch15/exercises.js
+++ Ch15/exercises.js
@@ -34,20 +34,50 @@
 
 <body>
 <script>
+  var arrowCodes = {37: "left", 38: "up", 39: "right"};
+  function trackKeys(codes) {
+  var pressed = Object.create(null);
+  function handler(event) {
+    if (codes.hasOwnProperty(event.keyCode)) {
+      var down = event.type == "keydown";
+      pressed[codes[event.keyCode]] = down;
+      event.preventDefault();
+    }
+  }
+  addEventListener("keydown", handler);
+  addEventListener("keyup", handler);
+  // give the returned object a function to clear the event listeners
+  pressed.reset = function() {
+    removeEventListener("keydown", handler);
+    removeEventListener("keyup", handler);
+  }
+  return pressed;
+}
   // The old runLevel function. Modify this...
   function runLevel(level, Display, andThen) {
     var display = new Display(document.body, level);
+
+    arrows = trackKeys(arrowCodes);
+
+    // determine whether or not to run the animation
     var shouldPlay = true;
-    addEventListener('keydown', function(e) {
+    function determinePlay(e) {
       if (e.keyCode == 27)
         shouldPlay = !shouldPlay;
-    });
+    }
+    addEventListener('keydown', determinePlay);
+
     runAnimation(function(step) {
+      // determine whether or not to animate
       if (shouldPlay)
         level.animate(step, arrows);
+
       display.drawFrame(step);
       if (level.isFinished()) {
         display.clear();
+        // remove our event listeners
+        removeEventListener('keydown', determinePlay);
+        arrows.reset();
         if (andThen)
           andThen(level.status);
         return false;
